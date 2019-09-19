bf34afc6df45b1766b3be02d0752ae1b02667879
Ruoshui <rmao10@stuy.edu>
2019-09-19 15:37:04 -0400

    add array and update pointer arithmetic files

--- pointer_arith.c
+++ pointer_arith.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+void bin(char n);
+
 int main(void) {
     // unsigned int *i = malloc(sizeof(int));
     // *i = 261;
     // int *p = i;
     // char *c = i;
 
-    unsigned int i = 263;
+    // 0x4C5FE314 = 01001100 01011111 11100011 00010100
+    // unsigned int i = 1281352468;
+    unsigned int i = 260;
     int *p = &i;
     char *c = &i;
     printf("p: %i at %p\n", *p, &p);
@@ -16,14 +20,25 @@ int main(void) {
 
 
     // print every byte of i
+    printf("\nprint every byte of i:\n");
     for (int x = 0; x < sizeof(unsigned int); x++, c++) {
         printf("%d byte of i is %hhu (uint), %hhx (uhex) at %p\n", x, *c, *c, c);
     }
 
+    // add 1 to every byte of i:
+    printf("\nadd 1 to every byte of i:\n");
     for (int x = 0; x < sizeof(unsigned int); x++, c++) {
-        *c = *c + 1;
-        printf("%d byte of i is %hhu (uint), %hhx (uhex) at %p\n", x, *c, *c, c);
-
+        c[x]++;
+        printf("%d byte of i is %hhu (uint), %hhx (uhex) at %p\n", x, c[x], c[x], c);
         printf("i (hex): %x, (int): %u\n", i, i);
     }
+
+
+}
+
+void bin(char n)
+{
+    if (n > 1)
+    bin(n>>1);
+    printf("%d", n & 1);
 }
